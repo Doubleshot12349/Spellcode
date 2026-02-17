2388cb3391749f6af1939326553a65814e77d904
Brett Thompson <thombret@oregonstate.edu>
2026-02-16 20:51:56 -0800

    Folder restructuring for organization and clarity. Misc metadata changes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Runtime.Serialization;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+    public int health;
+    private int mana;
+
+    public GameObject spell1;
+    public GameObject spell2;
+    public GameObject spell3;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void Cast(int spellNum)
+    {
+        GameObject []spells = { spell1, spell2, spell3 };
+    }
+}
