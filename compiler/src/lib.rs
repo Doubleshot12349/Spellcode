d6bb5fc1ece994695148da2bef466d3026d8fc27
Max <35409580+blahblahbloopster@users.noreply.github.com>
2026-02-28 17:51:03 -0800

    wip FFI

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,14 +1,75 @@
 #![feature(box_patterns)]
 
+use std::{ffi::{CStr, CString}, sync::Mutex};
+
 use crate::{compiler::Compiler, stack_machine::VM};
 
 mod stack_machine;
 mod parser;
 mod compiler;
 
+struct VMs {
+    vms: Vec<(i64, VM)>,
+    next_id: i64
+}
+
+static VMS: Mutex<VMs> = Mutex::new(VMs { vms: vec![], next_id: 0 });
+
+#[derive(Clone, Copy)]
+pub struct CompileResult {
+    id: i64,
+    error: *mut i8
+}
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn free_compileresult(inp: *const CompileResult) {
+    let v = unsafe { *inp };
+    let mut vms = VMS.lock().unwrap();
+    vms.vms.retain(|x| x.0 != v.id);
+
+    if !v.error.is_null() {
+        drop(unsafe { CString::from_raw(v.error) });
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn compile(program: *const i8) -> *const CompileResult {
+    let inp = unsafe { CStr::from_ptr(program) }.to_string_lossy();
+    let mut res = Box::new(CompileResult { id: -1, error: std::ptr::null_mut() });
+    let parsed = match parser::spellcode::program(&inp) {
+        Ok(v) => v,
+        Err(e) => {
+            let error = CString::new(format!("{e:?}")).unwrap();
+            res.error = error.into_raw();
+
+            return unsafe { std::mem::transmute(res) }
+        }
+    };
+        
+    let mut compiler = Compiler::new();
+    if let Err(e) = compiler.compile_program(&parsed) {
+        let error = CString::new(format!("{e:?}")).unwrap();
+        res.error = error.into_raw();
+
+        return unsafe { std::mem::transmute(res) }
+    }
+    let mut vms = VMS.lock().unwrap();
+    let id = vms.next_id;
+    vms.next_id += 1;
+    vms.vms.push((id, VM::new(compiler.program)));
+
+    unsafe { std::mem::transmute(res) }
+}
+
+
+//#[unsafe(no_mangle)]
+//pub extern "C" fn execute(left: u64, right: u64) -> u64 {
+//}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn add(left: u64, right: u64) -> u64 {
-    left + right
+    left + right + 1
 }
 
 fn main() {
